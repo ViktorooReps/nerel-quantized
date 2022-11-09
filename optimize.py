b67f2534c3de3d21615741f66e14ffbaacbcae29
viktoroo <viktoroo.sch@gmail.com>
2022-11-09 21:32:05 +0300

    cuda transfer + log

--- optimize.py
+++ optimize.py
@@ -1,10 +1,15 @@
 from pathlib import Path
 
+import torch.cuda
+
 from quant.model import SpanNERModel
 
 if __name__ == '__main__':
     model = SpanNERModel.load(Path('model_10epochs_cpu.pkl'))
+    model.to(torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'))
     model.prune(Path('data'), 0.2)
+    model.cpu()
     model.save(Path('model_10epochs_cpu_pruned.pkl'))
+    print('Model pruned and saved!')
     model.optimize(Path('onnx'), fuse=False, quant=True)
     model.save(Path('onnx/main.pkl'))
